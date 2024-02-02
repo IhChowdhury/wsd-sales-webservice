c32113e40d32f1fbc03b012e395ff0701c8d81dd
Ibrahim Chowdhury <ihchowdhury32@gmail.com>
2024-02-02 15:35:48 +0600

    Store log and add log interceptor to store request and response

---
+++ src/main/java/com/wsdtest/salesservice/LoggingInterceptor.java
@@ -0,0 +1,21 @@
+package com.wsdtest.salesservice;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoggingInterceptor implements HandlerInterceptor {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("Received request: {} {} from {}", request.getMethod(), request.getRequestURI(), request.getRemoteAddr());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        logger.info("Sent response: {} {} with status {} and exception {}", request.getMethod(), request.getRequestURI(), response.getStatus(), ex != null ? ex.getMessage() : null);
+    }
+}
